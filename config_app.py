4c66eca8abb9bd0219f302b0ee00e127486b479f
Gabriel Yllescas <gabaledan@gmail.com>
2026-05-16 12:18:31 -0500

    v01

---
+++ config_app.py
@@ -0,0 +1,9 @@
+def iniciar_sistema():
+    print("=== SISTEMA DE GESTIÓN INTEGRADO ===")
+    # CONFIGURACIÓN CRÍTICA: Definir el entorno de ejecución
+    entorno = "DESARROLLO_LOCAL"
+
+    print(f"[OK] Conectando a la base de datos en modo: {entorno}")
+    print("=== Sistema listo para recibir peticiones ===")
+
+iniciar_sistema()
