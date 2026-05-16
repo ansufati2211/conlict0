8d480239ecc1422a12ab517ccd9cb617d7a3af27
Jesus9Rojas <u23227101@utp.edu.pe>
2026-05-16 15:17:07 -0500

    Se cambio el entorno a la nube

--- config_app.py
+++ config_app.py
@@ -1,7 +1,7 @@
 def iniciar_sistema():
     print("=== SISTEMA DE GESTIÓN INTEGRADO ===")
-    # CONFIGURACIÓN CRÍTICA: Definir el entorno de ejecución
-    entorno = "DESARROLLO_LOCAL"
+    # CONFIGURACIÓN CRÍTICA: Definir el entorno de ejecución "entorno = "PRODUCCION_CLOUD"""
+    entorno = "PRODUCCION_CLOUD"
 
     print(f"[OK] Conectando a la base de datos en modo: {entorno}")
     print("=== Sistema listo para recibir peticiones ===")
