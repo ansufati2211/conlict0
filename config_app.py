be95e7ccbf71fdb11b730a621245273ac70e2b4d
Ariel <ariellegua2204@gmail.com>
2026-05-16 15:23:07 -0500

    servidor de test

--- config_app.py
+++ config_app.py
@@ -1,7 +1,7 @@
 def iniciar_sistema():
     print("=== SISTEMA DE GESTIÓN INTEGRADO ===")
     # CONFIGURACIÓN CRÍTICA: Definir el entorno de ejecución
-    entorno = "DESARROLLO_LOCAL"
+    entorno = "Testing_sever_development"
 
     print(f"[OK] Conectando a la base de datos en modo: {entorno}")
     print("=== Sistema listo para recibir peticiones ===")
